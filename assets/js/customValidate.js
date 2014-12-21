50ffb17af9bc59ec61b776e37567a0e60ce2737e
Poorna Krishnamoorthy <k.poorna@gmail.com>
2014-12-21 12:55:55 -0800

    added underscore.js and form validation with jquery

---
+++ assets/js/customValidate.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $('.form-signin').validate({
+      rules: {
+         firstname: {
+           required: true
+         },
+         lastname: {
+           required: true
+         },
+         email: {
+           required: true,
+           email : true
+         },
+         password: {
+           required: true,
+           minlength: 2
+         },
+         confirmation: {
+           minlength: 2,
+           equalTo: '#password'
+         }
+      },
+      success: function(element) {
+        element
+        .text('OK!').addClass('valid')
+      },
+      messages: {
+
+      }
+  });
+});
