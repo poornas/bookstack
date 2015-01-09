614c172ae1082ff82ff208827befd9f332853d88
Poorna Krishnamoorthy <k.poorna@gmail.com>
2015-01-09 10:57:04 -0800

    added bootstrap,less, masonry packages; org directories

---
+++ client/views/books/books_list.js
@@ -0,0 +1,26 @@
+var booksData = [ {
+title: 'Introducing Telescope',
+author: 'Sacha Greif',
+url: 'http://sachagreif.com/introducing-telescope/'
+}, 
+{
+title: 'Meteor',
+author: 'Tom Coleman',
+url: 'http://meteor.com'
+}, {
+	title: 'The Meteor Book', author: 'Tom Coleman',
+url: 'http://themeteorbook.com'
+} ];
+Template.booksList.helpers({ books: booksData
+});
+
+$.getScript('//cdn.jsdelivr.net/isotope/1.5.25/jquery.isotope.min.js',function(){
+
+  /* activate jquery isotope */
+  $('#books').imagesLoaded( function(){
+    $('#books').isotope({
+      itemSelector : '.item'
+    });
+  });
+  
+});
