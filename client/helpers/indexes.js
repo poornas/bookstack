1e8c7671fb4af9987783826507e105b212538dab
Poorna Krishnamoorthy <k.poorna@gmail.com>
2015-01-23 12:27:40 -0800

    clicking on tag to show bookslist filtered by tagname

--- client/helpers/indexes.js
+++ client/helpers/indexes.js
@@ -1,3 +1,9 @@
 Template.registerHelper('indexes', function() {
      return ['books', 'tags'];
 });
+
+Template.registerHelper('booksForTag', function(tagName) {
+	var matchingBookIds = Tags.find({'name': tagName},
+		 	       {'bookId':1,'_id':0}).map(function(u){return u.bookId;}).toArray();
+	return Books.find( { _id : { $in : matchingBookIds} });
+});
